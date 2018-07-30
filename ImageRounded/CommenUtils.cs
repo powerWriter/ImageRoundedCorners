b3ad8fcea1a5971fea7be6d646e06a09ee766233
Your Name <714327941@qq.com>
2018-07-30 11:02:46 +0800

    首次提交文件

---
+++ ImageRounded/CommenUtils.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ImageRounded
+{
+    class CommenUtils
+    {
+        /// <summary>
+        /// 圆角生成（但是只能是一个角）
+        /// </summary>
+        /// <param name="image">源图片 Image</param>
+        /// <param name="roundCorner">圆角位置</param>
+        /// <returns>处理好的Image</returns>
+        public static System.Drawing.Image CreateRoundedCorner(System.Drawing.Image image, string radius, RoundRectanglePosition roundCorner)
+        {
+            Graphics g = Graphics.FromImage(image);
+            //保证图片质量
+            g.SmoothingMode = SmoothingMode.HighQuality;
+            g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            g.CompositingQuality = CompositingQuality.HighQuality;
+            Rectangle rect = new Rectangle(0, 0, image.Width, image.Height);
+            //构建圆角外部路径
+            GraphicsPath rectPath = CreateRoundRectanglePath(rect, int.Parse(radius), roundCorner);
+            PointF[] p = rectPath.PathPoints;
+            //PathData pathData = rectPath.PathData;
+            //圆角背景用白色填充
+            Brush b = new SolidBrush(Color.White);
+            g.DrawPath(new Pen(b), rectPath);
+            g.FillPath(b, rectPath);
+            g.Dispose();
+            return image;
+        }
+
+        /// <summary>
+        /// 在圆角边缘的判断需要改进，相切部分做一下虚化处理
+        /// </summary>
+        /// <param name="bit"></param>
+        /// <param name="RoundCornerSize"></param>
+        /// <returns></returns>
+        public static Bitmap doOperation(Bitmap bit,int RoundCornerSize)
+        {
+
+            int w1 = bit.Width;
+            int h1 = bit.Height;
+
+
+            //左上角
+            for (int y = 0; y < RoundCornerSize; y++)
+            {
+                for (int x = 0; x < RoundCornerSize; x++)
+                {
+                    double aX = RoundCornerSize - x;
+                    double bY = RoundCornerSize - y;
+                    double c = Math.Sqrt(Math.Pow(aX,2) + Math.Pow(bY,2));
+                    if (c > RoundCornerSize)//圆标准方程
+                    {
+                        //将圆以外的点设为透明
+                        bit.SetPixel(x, y, System.Drawing.Color.FromArgb(0, 255, 255, 255));
+                    }
+                    if ((2 > (c - RoundCornerSize) && (c - RoundCornerSize) > 0) || c == RoundCornerSize)//圆标准方程
+                    {
+                        //将圆以外的点设为透明
+                        bit.SetPixel(x, y, System.Drawing.Color.FromArgb(10, 128, 128, 128));
+                    }
+                }
+            }
+            // 右上角
+            for (int x = w1 - RoundCornerSize; x < w1; x++)
+            {
+                for (int y = 0; y < RoundCornerSize; y++)
+                {
+                    double aX = x - (w1 - RoundCornerSize);
+                    double bY = RoundCornerSize - y;
+                    double c = Math.Sqrt(Math.Pow(aX, 2) + Math.Pow(bY, 2));
+                    if (c > RoundCornerSize)//圆标准方程
+                    {
+                        //将圆以外的点设为透明
+                        bit.SetPixel(x, y, System.Drawing.Color.FromArgb(0, 255, 255, 255));
+                    }
+                    if ((2 > (c - RoundCornerSize) && (c - RoundCornerSize) > 0 ) || c == RoundCornerSize)//圆标准方程
+                    {
+                        //将圆以外的点设为透明
+                        bit.SetPixel(x, y, System.Drawing.Color.FromArgb(12, 128, 128, 128));
+                    }
+                }
+            }
+            //左下角
+            for (int x = 0; x < RoundCornerSize; x++)
+            {
+                for (int y = h1 - RoundCornerSize; y < h1; y++)
+                {
+                    double aX = RoundCornerSize - x;
+                    double bY = y - (h1 - RoundCornerSize);
+                    double c = Math.Sqrt(Math.Pow(aX, 2) + Math.Pow(bY, 2));
+                    if (c > RoundCornerSize)//圆标准方程
+                    {
+                        //将圆以外的点设为透明
+                        bit.SetPixel(x, y, System.Drawing.Color.FromArgb(0, 255, 255, 255));
+                    }
+                    if ((2 > (c - RoundCornerSize) && (c - RoundCornerSize) > 0) || c == RoundCornerSize)//圆标准方程
+                    {
+                        //将圆以外的点设为透明
+                        bit.SetPixel(x, y, System.Drawing.Color.FromArgb(12, 128, 128, 128));
+                    }
+                }
+            }
+            //右下角
+            for (int x = w1 - RoundCornerSize; x < w1; x++)
+            {
+                for (int y = h1 - RoundCornerSize; y < h1; y++)
+                {
+                    double aX = x - (w1 - RoundCornerSize);
+                    double bY = y - (h1 - RoundCornerSize);
+                    double c = Math.Sqrt(Math.Pow(aX, 2) + Math.Pow(bY, 2));
+                    if (c > RoundCornerSize)//圆标准方程
+                    {
+                        //将圆以外的点设为透明
+                        bit.SetPixel(x, y, System.Drawing.Color.FromArgb(0, 255, 255, 255));
+                    }
+                    if ((2 > (c - RoundCornerSize) && (c - RoundCornerSize) > 0) || c == RoundCornerSize)//圆标准方程
+                    {
+                        //将圆以外的点设为透明
+                        bit.SetPixel(x, y, System.Drawing.Color.FromArgb(12, 128, 128, 128));
+                    }
+                }
+
+            }
+            return bit;
+        }
+
+
+        /// <summary>
+        /// 目标图片的圆角位置
+        /// </summary>
+        public enum RoundRectanglePosition
+        {
+            /// <summary>
+            /// 左上角
+            /// </summary>
+            TopLeft,
+            /// <summary>
+            /// 右上角
+            /// </summary>
+            TopRight,
+            /// <summary>
+            /// 左下角
+            /// </summary>
+            BottomLeft,
+            /// <summary>
+            /// 右下角
+            /// </summary>
+            BottomRight
+        }
+        /// <summary>
+        /// 构建GraphicsPath路径
+        /// </summary>
+        /// <param name="rect"></param>
+        /// <param name="radius"></param>
+        /// <param name="rrPosition">图片圆角位置</param>
+        /// <returns>返回GraphicPath</returns>
+        private static GraphicsPath CreateRoundRectanglePath(Rectangle rect, int radius, RoundRectanglePosition rrPosition)
+        {
+            GraphicsPath rectPath = new GraphicsPath();
+            switch (rrPosition)
+            {
+                case RoundRectanglePosition.TopLeft:
+                    {
+                        rectPath.AddArc(rect.Left, rect.Top, radius * 2, radius * 2, 180, 90);
+                        rectPath.AddLine(rect.Left, rect.Top, rect.Left, rect.Top + radius);
+                        break;
+                    }
+                case RoundRectanglePosition.TopRight:
+                    {
+                        rectPath.AddArc(rect.Right - radius * 2, rect.Top, radius * 2, radius * 2, 270, 90);
+                        rectPath.AddLine(rect.Right, rect.Top, rect.Right - radius, rect.Top);
+                        break;
+                    }
+                case RoundRectanglePosition.BottomLeft:
+                    {
+                        rectPath.AddArc(rect.Left, rect.Bottom - radius * 2, radius * 2, radius * 2, 90, 90);
+                        rectPath.AddLine(rect.Left, rect.Bottom - radius, rect.Left, rect.Bottom);
+                        break;
+                    }
+                case RoundRectanglePosition.BottomRight:
+                    {
+                        rectPath.AddArc(rect.Right - radius * 2, rect.Bottom - radius * 2, radius * 2, radius * 2, 0, 90);
+                        rectPath.AddLine(rect.Right - radius, rect.Bottom, rect.Right, rect.Bottom);
+                        break;
+                    }
+            }
+            return rectPath;
+        }
+
+
+        /// <summary>
+        /// 图片缩放
+        /// </summary>
+        /// <param name="b">源图片Bitmap</param>
+        /// <param name="dstWidth">目标宽度</param>
+        /// <param name="dstHeight">目标高度</param>
+        /// <returns>处理完成的图片 Bitmap</returns>
+        public static Bitmap ResizeBitmap(Bitmap b, int dstWidth, int dstHeight)
+        {
+            Bitmap dstImage = new Bitmap(dstWidth, dstHeight);
+            System.Drawing.Graphics g = System.Drawing.Graphics.FromImage(dstImage);
+            //   设置插值模式 
+            g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.Bilinear;
+            //   设置平滑模式 
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            g.SmoothingMode = SmoothingMode.HighQuality;
+            g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            //用Graphic的DrawImage方法通过设置大小绘制新的图片实现缩放
+            g.DrawImage(b, new Rectangle(0, 0, dstImage.Width, dstImage.Height), new Rectangle(0, 0, b.Width, b.Height), GraphicsUnit.Pixel);
+            g.Save();
+            g.Dispose();
+            return dstImage;
+        }
+
+
+    }
+}
---
+++ ImageRounded/MainWindow.xaml.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Windows;
+using System.Drawing;
+using System.Windows.Media.Imaging;
+using System.IO;
+using System.Drawing.Imaging;
+using System.Windows.Media;
+
+namespace ImageRounded
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private bool changeTag = false;//是否已执行转换操作 true-已执行，false-未执行
+        public Bitmap doBitmap ;
+        public MainWindow()
+        {
+
+        }
+
+        private void SelectImage_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "图片文件(*.jpg,*.png)|*.jpg;*.png;*.jpeg";//规定指定的格式
+            dialog.Multiselect = false;//是否可以多选
+            if (dialog.ShowDialog() == true)
+            {
+                //string now1 = DateTime.Now.ToString("yyyyMMddHHmmssf");
+                foreach (String fileName in dialog.FileNames)
+                {
+                    using (Bitmap bitImage = new Bitmap(fileName))
+                    {
+                        FilePath.Text = fileName;
+                        doBitmap = bitImage;
+                        BitmapImage bitmapImage = BitmapToBitmapImageFromBitmap(bitImage);
+                        ImagePreview.Source = bitmapImage;
+                    }
+                }
+            }
+        }
+
+        private void OutPutPath_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            System.Windows.Forms.FolderBrowserDialog folder = new System.Windows.Forms.FolderBrowserDialog();
+            if (folder.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                OutPutPath.Text = folder.SelectedPath;
+            }
+        }
+
+        private void OperationBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (RoundCornerSize.Text.Trim() == "")
+            {
+                MessageBox.Show("请输入圆角大小");
+                return;
+            }
+            if (null == ImagePreview.Source)
+            {
+                MessageBox.Show("请先导入图像");
+                return;
+            }
+
+            Image tempImg = ImageSourceToBitmap(ImagePreview.Source);
+            tempImg = CommenUtils.CreateRoundedCorner(tempImg, RoundCornerSize.Text,CommenUtils.RoundRectanglePosition.TopLeft);
+            tempImg = CommenUtils.CreateRoundedCorner(tempImg, RoundCornerSize.Text,CommenUtils.RoundRectanglePosition.TopRight);
+            tempImg = CommenUtils.CreateRoundedCorner(tempImg, RoundCornerSize.Text,CommenUtils.RoundRectanglePosition.BottomLeft);
+            tempImg = CommenUtils.CreateRoundedCorner(tempImg, RoundCornerSize.Text,CommenUtils.RoundRectanglePosition.BottomRight);
+
+            ImagePreview.Source = BitmapToBitmapImageFromBitmap((Bitmap)tempImg);
+        }
+
+        private void OutPutBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if(OutPutPath.Text.Trim() == "")
+            {
+                MessageBox.Show("请输入输出路径");
+                return;
+            }
+            if (OutPutName.Text.Trim() == "")
+            {
+                MessageBox.Show("请输入输出文件名");
+                return;
+            }
+
+            Image tempImg = ImageSourceToBitmap(ImagePreview.Source);
+
+            if (comboBox.SelectedIndex == 0)
+                tempImg.Save(OutPutPath.Text + "\\" + OutPutName.Text + ".png", ImageFormat.Png);
+            else if (comboBox.SelectedIndex == 1)
+            {
+                Bitmap bitmapImage = CommenUtils.doOperation((Bitmap)tempImg, int.Parse(RoundCornerSize.Text));
+                bitmapImage.Save(OutPutPath.Text + "\\" + OutPutName.Text + ".png", ImageFormat.Png);
+            }
+             
+            MessageBox.Show("导出成功！","提示信息");
+        }
+
+        public static BitmapImage BitmapToBitmapImageFromBitmap(Bitmap bitmap)
+        {
+            #region
+            BitmapImage bitmapImage = new BitmapImage();
+            using (System.IO.MemoryStream ms = new System.IO.MemoryStream())
+            {
+                bitmap.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = new MemoryStream(ms.ToArray());
+                bitmapImage.EndInit();
+            }
+            return bitmapImage;
+            #endregion
+        }
+
+        /// <summary>
+        /// ImageSource to Bitmap
+        /// </summary>
+        /// <param name="imageSource"></param>
+        /// <returns></returns>
+        public static System.Drawing.Bitmap ImageSourceToBitmap(ImageSource imageSource)
+        {
+            BitmapSource m = (BitmapSource)imageSource;
+
+            System.Drawing.Bitmap bmp = new System.Drawing.Bitmap(m.PixelWidth, m.PixelHeight, System.Drawing.Imaging.PixelFormat.Format32bppPArgb); // 坑点：选Format32bppRgb将不带透明度
+
+            System.Drawing.Imaging.BitmapData data = bmp.LockBits(
+            new System.Drawing.Rectangle(System.Drawing.Point.Empty, bmp.Size), System.Drawing.Imaging.ImageLockMode.WriteOnly, System.Drawing.Imaging.PixelFormat.Format32bppPArgb);
+
+            m.CopyPixels(Int32Rect.Empty, data.Scan0, data.Height * data.Stride, data.Stride);
+            bmp.UnlockBits(data);
+
+            return bmp;
+        }
+
+    }
+}
